c91618d9d7086108fe0fe50af3e74b74353f7551
Bitan Atarthy <bitan.atarthy07@gmail.com>
2024-03-07 17:14:34 +0530

    minor changes

--- src/components/ComingSoon.jsx
+++ src/components/ComingSoon.jsx
@@ -1,17 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const ComingSoon = () => {
   return (
     <div className="w-full min-h-90 text-center relative bg-transparent">
       <div className="absolute inset-0 flex flex-col justify-center items-center">
-        <p className=" leading-none text-amber-500 animate-text text-8xl md:text-6xl  z-10 ">
-          <span className='SanskritiFont font-extrabold text-8xl md:text-9xl'>Coming Soon... </span>
-          🙏🏻🛕
-        </p> 
-        
+        <p className=" leading-none text-amber-500 animate-text SanskritiFont font-extrabold text-6xl md:text-9xl  z-10 ">
+          Coming Soon...
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ComingSoon
+export default ComingSoon;
